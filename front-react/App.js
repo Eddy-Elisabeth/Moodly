a32f8e0eb5b4784e4f29a0a52a5cc32a227f514a
Eddy-Elisabeth <eddy.elisabeth@epitech.digital>
2024-10-23 20:26:25 +0200

    ajout page welcome

--- front-react/App.js
+++ front-react/App.js
@@ -20,6 +20,34 @@
 // });
 
 
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import LoginScreen from './screens/LoginScreen';
+// import RegisterScreen from './screens/RegisterScreen';
+// import HomeScreen from './screens/HomeScreen';
+// import MoodScreen from './screens/MoodScreen';
+// import 'react-native-gesture-handler';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Login">
+//         <Stack.Screen name="Login" component={LoginScreen} />
+//         <Stack.Screen name="Register" component={RegisterScreen} />
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Mood" component={MoodScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+
+
+
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -27,6 +55,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
 import MoodScreen from './screens/MoodScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
 import 'react-native-gesture-handler';
 
 const Stack = createNativeStackNavigator();
@@ -34,7 +63,12 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen 
+          name="Welcome" 
+          component={WelcomeScreen} 
+          options={{ headerShown: false }} 
+        />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
@@ -43,3 +77,6 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+
+
